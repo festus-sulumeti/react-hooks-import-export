9ee451a18508950bc9e0639175ac4b9aa65ddbf1
festus-sulumeti <sulumetifestus@outlook.com>
2023-11-03 10:06:07 +0300

    THe completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import ColoradoStateParks from "./ColoradoStateParks";
+import MesaVerde from "./parks/MesaVerde";
 
-ReactDOM.render(<ColoradoStateParks />, document.getElementById("root"));
+ReactDOM.render(
+    <di>
+        <ColoradoStateParks />
+        <MesaVerde />
+    </di>,
+ document.getElementById("root"));
